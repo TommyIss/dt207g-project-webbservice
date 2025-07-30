bd4adf2888804444c2eb42ca2eefba13c2143f6c
Tommy Issa <tommyissa@hotmail.com>
2025-07-31 00:52:30 +0200

    Har skapat anslutning till databas, samt en tabell för maträtter

---
+++ install.js
@@ -0,0 +1,38 @@
+/**
+ * Installation-fil för tabell i databas
+ */
+let { Client } = require('pg');
+require('dotenv').config();
+
+// Anslut till databas
+let client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect((err) => {
+    if(err) {
+        console.log('Fel vid anslutning till databas: ' + err);
+    } else {
+        console.log('Ansluten till databas');
+    }
+});
+
+client.query(`
+    DROP TABLE IF EXISTS meals;
+    CREATE TABLE meals(
+        id SERIAL PRIMARY KEY,
+        meal_name TEXT,
+        meal_type TEXT,
+        ingredients JSONB,
+        price DECIMAL(6, 2),
+        added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+`);
+
